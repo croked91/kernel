03a64d08238333fd04ab53026ffa101351b07c39
Egor Tikhomirov <atik.egor@gmail.com>
2024-12-25 19:33:34 +0300

    Update kernel.c

--- kernel.c
+++ kernel.c
@@ -1,111 +1,89 @@
+#include <sys/io.h>
+
 #define WHITE_TXT_ON_BLUE_BG 0x1F 
 #define WHITE_TXT_ON_GREEN_BG 0x2F 
-#define LINE_SIZE 160 // 80 символов на строку по 2 байта
+#define LINE_SIZE 80*2
 #define LINE_COUNT 25
+#define VIDEO_MEMORY 0xb8000
+#define KEYBOARD_PORT 0x60
+#define CURSOR_COMMAND_PORT 0x3D4
+#define CURSOR_DATA_PORT 0x3D5
+#define SPEAKER_PORT 0x61
 
 void k_clear_screen(int color);
-unsigned int k_printf(char *message, unsigned int line, int text_color);
+void k_printf(const char *message, unsigned int line, int text_color);
 void k_get_text(char *buffer, int pos, unsigned int max_length);
 void k_update_cursor(int row, int col);
-static inline void k_outb(unsigned short port, unsigned char value);
-static inline unsigned char k_inb(unsigned short port);
 
 void k_main() 
 {
     char password[4];
 
     k_clear_screen(WHITE_TXT_ON_BLUE_BG);
-    k_printf("Hello! It's your bank security service.", 0, WHITE_TXT_ON_BLUE_BG);
-    k_printf("Send money to our secret deposit and paste the any password.", 1, WHITE_TXT_ON_BLUE_BG);
-    k_printf("Enter password:", 3, WHITE_TXT_ON_BLUE_BG);
-		k_update_cursor(3, 15);
+    k_printf("GIVE ME YOUR DENGI", 0, WHITE_TXT_ON_BLUE_BG);
+    k_printf("OUR SCHET IS 0000 0000 0000 0000", 1, WHITE_TXT_ON_BLUE_BG);
+    k_printf("ENTER PASSWORD:", 3, WHITE_TXT_ON_BLUE_BG);
+    k_update_cursor(3, 15);
 
-    k_outb(0x61, k_inb(0x61) | 0x03); 
+    outb(inb(SPEAKER_PORT) | 0x03, SPEAKER_PORT); 
 
-		int pos = 3 * LINE_SIZE + 15 * 2;
-    k_get_text(password, pos, 4);
+    int pos = 3 * LINE_SIZE + 15 * 2;
+    k_get_text(password, pos, sizeof(password));
 
- 		k_outb(0x61, k_inb(0x61) & 0xFC);
-		k_clear_screen(WHITE_TXT_ON_GREEN_BG);
-    k_printf("Password received!", 0, WHITE_TXT_ON_GREEN_BG);
-		k_printf("Thank you for your trust!", 1, WHITE_TXT_ON_GREEN_BG);
-		k_printf("Please rate our scam service on www.its-really-truth.net", 2, WHITE_TXT_ON_GREEN_BG);
-		k_update_cursor(3, 0);
+    outb(inb(SPEAKER_PORT) & 0xFC, SPEAKER_PORT);
+    k_clear_screen(WHITE_TXT_ON_GREEN_BG);
+    k_printf("THANK YOU!", 0, WHITE_TXT_ON_GREEN_BG);
+    k_update_cursor(0, 9);
 
     while (1) {}
-};
-
-static inline unsigned char k_inb(unsigned short port) {
-    unsigned char result;
-    __asm__ __volatile__("inb %1, %0" : "=a"(result) : "d"(port));
-    return result;
-};
-
-static inline void k_outb(unsigned short port, unsigned char value) {
-    __asm__ __volatile__("outb %0, %1" : : "a"(value), "d"(port));
-};
+}
 
 void k_clear_screen(int color)
 {
-    char *vidmem = (char *) 0xb8000;
-    unsigned int i = 0;
-    while (i < (LINE_SIZE * LINE_COUNT))
-    {
-        vidmem[i] = ' ';              
-        i++;
-        vidmem[i] = color;
-        i++;
-    };
-};
+    char *vidmem = (char *)VIDEO_MEMORY;
+    for (unsigned int i = 0; i < (LINE_SIZE * LINE_COUNT); i += 2) {
+        vidmem[i] = ' ';               
+        vidmem[i + 1] = color;
+    }
+}
 
-unsigned int k_printf(char *message, unsigned int line, int text_color)
+void k_printf(const char *message, unsigned int line, int text_color)
 {
-    char *vidmem = (char *) 0xb8000;
-    unsigned int i = 0;
+    char *vidmem = (char *)VIDEO_MEMORY;
+    unsigned int i = line * LINE_SIZE;
 
-    i = (line * LINE_SIZE);
-
-    while (*message != 0)
-    {
-        if (*message == '\n') 
-        {
+    while (*message) {
+        if (*message == '\n') {
             line++;
-            i = (line * LINE_SIZE);
-            message++;
+            i = line * LINE_SIZE;
+        } else {
+            vidmem[i++] = *message;
+            vidmem[i++] = text_color;
         }
-        else
-        {
-            vidmem[i] = *message;              
-            message++;
-            i++;
-            vidmem[i] = text_color;
-            i++;
-        };
-    };
-
-    return 1;
-};
+        message++;
+    }
+}
 
 void k_update_cursor(int row, int col)
 {
-    unsigned short position=(row * 80) + col;
+    unsigned short position = (row * 80) + col;
 
-    k_outb(0x3D4, 14);
-    k_outb(0x3D5, (unsigned char)(position>>8));
-    k_outb(0x3D4, 15);
-    k_outb(0x3D5, (unsigned char)(position));
-};
+    outb(14, CURSOR_COMMAND_PORT);
+    outb((unsigned char)(position >> 8), CURSOR_DATA_PORT);
+    outb(15, CURSOR_COMMAND_PORT);
+    outb((unsigned char)position, CURSOR_DATA_PORT);
+}
 
 void k_get_text(char *buffer, int pos, unsigned int max_length)
 {
     unsigned int count = 0;
-    char *vidmem = (char *) 0xb8000;
+    char *vidmem = (char *)VIDEO_MEMORY;
 
     while (1) {
         unsigned char scancode;
 
         do {
-            scancode = k_inb(0x60);
+            scancode = inb(KEYBOARD_PORT);
         } while (scancode & 0x80);
 
         char key = 0;
@@ -120,10 +98,10 @@ void k_get_text(char *buffer, int pos, unsigned int max_length)
             case 0x09: key = '8'; break;
             case 0x0A: key = '9'; break;
             case 0x0B: key = '0'; break;
-            case 0x1C: 
+            case 0x1C: // Enter key
                 buffer[count] = '\0';
                 return;
-            case 0x0E: 
+            case 0x0E: // Backspace
                 if (count > 0) {
                     count--;
                     pos -= 2;
@@ -135,16 +113,15 @@ void k_get_text(char *buffer, int pos, unsigned int max_length)
         }
 
         if (count < max_length) {
-            buffer[count] = key;  
+            buffer[count++] = key;  
             vidmem[pos] = key;    
             vidmem[pos + 1] = WHITE_TXT_ON_BLUE_BG;
-            count++;
             pos += 2;         
-						k_update_cursor(0, pos/2);
+            k_update_cursor(0, pos / 2);
         }
 
         do {
-            scancode = k_inb(0x60);
+            scancode = inb(KEYBOARD_PORT);
         } while (!(scancode & 0x80));
     }
-};
+}
